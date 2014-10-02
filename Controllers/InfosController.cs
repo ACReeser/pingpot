b85d7f5ded32a7b5d59e1526545f7847c76cc8ed
orthaanc <orthaanc@gmail.com>
2014-10-02 10:02:14 -0500

    Adding infosController and ElevensesModel

---
+++ Controllers/InfosController.cs
@@ -0,0 +1,45 @@
+﻿using pingpot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace pingpot.Controllers
+{
+    public class InfosController : ApiController
+    {
+        public static ElevensesModel Elevenses { get; set; }
+
+        static InfosController()
+        {
+            Elevenses = new ElevensesModel();
+        }
+
+        // GET api/<controller>
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, Elevenses);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("infos/1")]
+        public HttpResponseMessage FlipElevenses()
+        {
+            Elevenses.Elevenses = !Elevenses.Elevenses;
+            return Request.CreateResponse(HttpStatusCode.OK, Elevenses);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ models/ElevensesModel.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pingpot.Models
+{
+    public class ElevensesModel
+    {
+        [JsonProperty("elevenses")]
+        public bool Elevenses { get; set; }
+    }
+}
