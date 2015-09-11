9f2306e81d452212f8bcf00496c495b338af8842
acreeser <alexcreeser@gmail.com>
2015-09-11 17:02:35 -0500

    complete overhaul of UI, with persistence

--- scripts/app.ng.js
+++ scripts/app.ng.js
@@ -5,45 +5,16 @@ angular.module('myApp', [
 //  'ngRoute'
 ])
 .controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
-    var debug = window.location.port != 80 && window.location.port != 443;
-
-    if (debug) {
-        $scope.supplies = {
-            creamer: ['Alex', 'Landon', 'Ben'],
-            sugar: ['Alex', 'Landon', 'Ben'],
-            coffee: ['Alex', 'Landon', 'Ben'],
-        }
-    }
-
     $scope.pot = {
         cupsLeft: 12,
         fillLevel: 75,
         burnerClass: ""
     };
 
+    $scope.supplies = {};
+
     $scope.signalClass = {};
-
-    //#region chat
-
-    //$scope.chatting = false;
-    //$scope.nickname = "";
-    //$scope.allChats = "";
-
-    //var currentPeer;
-    //$scope.connectToChat = function () {
-    //    currentPeer = new peer($scope.nickname, function (newChat) {
-    //        $scope.allChats += newChat;
-    //    });
-    //    $scope.chatting = true;
-    //}
-
-    //$scope.send = function () {
-    //    $scope.newChat = "";
-    //    currentPeer.send($scope.newChat);
-    //}
-
-    //#endregion
-
+    
     var signalCounter = 0,
         signalling = false;
 
@@ -62,22 +33,22 @@ angular.module('myApp', [
     }
 
     function startSignalling() {
-        $scope.signalClass = { active: true };
+        $scope.signalClass = { one: true };
         $timeout(signalOne, 250);
     }
 
     function signalOne() {
-        $scope.signalClass.one = true;
+        $scope.signalClass.two = true;
         $timeout(signalTwo, 250);
     }
 
     function signalTwo() {
-        $scope.signalClass.two = true;
+        $scope.signalClass.three = true;
         $timeout(function () {
             if (!signalling) {
                 $scope.signalClass = {};
             } else {
-                $scope.signalClass = { active: true };
+                $scope.signalClass = { one: true };
                 $timeout(signalOne, 250);
             }
         }, 250);
@@ -122,7 +93,7 @@ angular.module('myApp', [
             if (data) {
                 var newPot = angular.fromJson(data.data);
                 $scope.pot = newPot;
-                updateBurner();
+                $timeout(updateBurner, 0);
                 updateFillLevel();
             }
         });
@@ -164,42 +135,30 @@ angular.module('myApp', [
         $http.post("api/pot/"+$scope.pot.Id, $scope.pot);
     };
 
+    //not implemented yet
     $scope.cycleSupply = function (person, type) {
         if (confirm(person + " brought in more " + type + "?")) {
             $http.post('/api/supplies?supplyType=' + type, { 'person': person }).success(pollSupplies);
         }
     };
 
-    var potModal = '<div id="pot-modal"><button class="btn-success" ng-click="hide()"></button></div>';
 
-    $scope.potModal = function () {
-        //do things
-    };
+    var muggySayings = [
+        "coffee is brain juice!",
+        "arabica = awesome!",
+        "how 'bout a cuppa joe?",
+        "where's the creamer at?",
+        "i'm a talking mug!",
+        "mooooooore coffeeeee",
+        "nectar of the gods!",
+        "empty mugs = sad mugs"
+    ];
+
+    $scope.showMuggySaying = function () {
+        $("#muggText").text(muggySayings[Math.floor(Math.random() * (muggySayings.length))]);
+        $timeout($scope.showMuggySaying, 5000);
+    }
+
+    $timeout($scope.showMuggySaying, 3000);
+
 }])
-//.factory('peer', [function () {
-//    var service = function (nickname, callback) {
-//        this.nickname = nickname;
-//        this.callback = callback;
-//        this.peer = new Peer('pingpot-chat-'+nickname, { key: 'k031yumxu75h4cxr' });
-//        var conn = this.peer.connect('pingpot-chat-alex');
-//        this.peer.on('open', function () {
-//            conn.send(this.nickname + ' has joined chat');
-//        }.bind(this));
-//        conn.on('data', function (data) {
-//            console.log(data);
-//            if (this.callback)
-//                this.callback(data);
-//        }.bind(this));
-
-//        this.connection = conn;
-//    };
-
-//    service.prototype.send = function (newChat) {
-//        this.connection.send(this.nickname + ": "+newChat);
-//    }
-
-//    return service;
-//}]);
-//.config(['$routeProvider', function($routeProvider) {
-//  $routeProvider.otherwise({redirectTo: '/view1'});
-//}]);
